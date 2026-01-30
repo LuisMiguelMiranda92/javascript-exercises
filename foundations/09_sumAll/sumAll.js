0d8e28372fc0720d2aaf535d7d9546640fa24f43
Luis Miguel Miranda <lmbasmiranda@gmail.com>
2026-01-30 09:25:22 -0800

    Practice js foundations

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if ((a < 0) | (b < 0)) return "ERROR";
+  if (!Number.isInteger(a) | !Number.isInteger(b)) return "ERROR";
 
+  let sum = 0;
+  let firstNum = 0;
+  let lastNum = 0;
+
+  if (a > b) {
+    firstNum = b;
+    lastNum = a;
+  } else {
+    firstNum = a;
+    lastNum = b;
+  }
+
+  for (let i = firstNum; i <= lastNum; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
