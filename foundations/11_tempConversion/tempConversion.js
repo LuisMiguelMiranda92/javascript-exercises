f56667f12f2ffe8271563af465a7481170f57656
Luis Miguel Miranda <lmbasmiranda@gmail.com>
2026-01-30 09:46:48 -0800

    Practice js foundations

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degrees) {
+  return Math.round((5 / 9) * (degrees - 32) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degrees) {
+  return Math.round(((9 / 5) * degrees + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
